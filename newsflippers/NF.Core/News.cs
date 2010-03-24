eb568da6525590016814cd972b41685a6fce49de
ludmal <ludmal@b3d67712-e981-11de-a700-c3e9af1e83fa>
2010-03-24 14:45:26 +0000

    New database project added to the reopository

---
+++ newsflippers/NF.Core/News.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Infonex.Data;
+using System.Data.SqlClient;
+
+namespace NF.Core
+{
+    public class News 
+    {
+        public static void Insert(int source, string title, string desc, string link, string author, string category, DateTime pubdate, DateTime extractedDate, string dateref, string imageName) {
+            Database db = new Database();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Parameters.AddWithValue("@NEWS_SOURCE", source );
+            cmd.Parameters.AddWithValue("@NEWS_TITLE", title );
+            cmd.Parameters.AddWithValue("@NEWS_DESC", desc );
+            cmd.Parameters.AddWithValue("@NEWS_LINK", link );
+            cmd.Parameters.AddWithValue("@NEWS_AUTHOR", author );
+            cmd.Parameters.AddWithValue("@NEWS_CATEGORY",category );
+            cmd.Parameters.AddWithValue("@NEWS_PUBDATE", pubdate );
+            cmd.Parameters.AddWithValue("@NEWS_EXTRACT_DATE",extractedDate );
+            cmd.Parameters.AddWithValue("@NEWS_DATEREF",dateref );
+            cmd.Parameters.AddWithValue("@NEWS_IMAGE_NAME",imageName );
+            cmd.Parameters.AddWithValue("@NEWS_IMAGE_GENERATED", false );
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
