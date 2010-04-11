39c7bfcb20eb2999cdaddce273b5092db0d892a3
ludmal <ludmal@b3d67712-e981-11de-a700-c3e9af1e83fa>
2010-04-11 02:09:47 +0000

    This brached version is the intial version before working on the NF_2--Which has alot of coding and advance features.

---
+++ branches/1.0.0/NF.Web/admin/default.aspx.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+using System.IO;
+
+namespace newsflippers.admin
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) {
+                this.Label1.Text = Utility.LocalDate().ToString(Utility.DATE_LONG_FORMAT);
+                if (Request["admin"] != null) {
+                    InsertData();
+                    Response.Redirect("~/default.aspx");
+                }
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List<Source> sources = NewsManager.GetAllSources();
+                StringBuilder b = new StringBuilder();
+                int count = 0;
+                foreach (Source s in sources)
+                {
+                        b.AppendFormat("{0}<br>", Extensions.FormatURL(s.Url));
+                        count++;
+                }
+                this.lblMsg.Text = string.Format("{0} links", count.ToString());
+                this.Label2.Text = b.ToString();
+                
+            }
+            catch (Exception ex)
+            {
+                this.lblMsg.Text = ex.Message;
+            }
+            
+        }
+
+        private void InsertData() {
+            string todayFolder = HttpContext.Current.Server.MapPath(Utility.GetImageFolder());
+            string dateTimeText = Extensions.ToNewsDateTime(Utility.LocalDate());
+
+            List<Source> sources = NewsManager.GetAllSources();
+
+            int count = 0;
+
+            foreach (Source s in sources)
+            {
+                string imgName = Extensions.ToImageString(s.Url);
+                if (IsImageExists(imgName))
+                {
+                    if (!NewsManager.IsImageExists(imgName + ".gif"))
+                    {
+                        NewsManager.InsertImage(s.ID, imgName + ".gif", "", Utility.FormatURL(s.Url.ToString()));
+                        count++;
+                    }
+                }
+            }
+
+            this.lblMsg.Text = string.Format("{0} inserted!", count.ToString());
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            InsertData();
+
+        }
+        private string GetValue(int i)
+        {
+            if (i < 10)
+            {
+                return string.Format("0{0}", i.ToString());
+            }
+            return i.ToString();
+        }
+
+        private bool IsImageExists(string imageName)
+        {
+            string todayFolder = HttpContext.Current.Server.MapPath(Utility.GetImageFolder());
+            return File.Exists(todayFolder + imageName + ".gif");
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            string path = Server.MapPath("~") + @"\pages\";
+            for (int i = 2010; i <= 2012; i++)
+            {
+                Directory.CreateDirectory(path + GetValue(i));
+                for (int j = 1; j <= 12; j++)
+                {
+                    string path1 = string.Format(@"{0}{1}\{2}", path, GetValue(i), GetValue(j));
+                    Directory.CreateDirectory(path1);
+                    for (int x = 1; x <= 31; x++)
+                    {
+                        string path2 = string.Format(@"{0}{1}\{2}\{3}", path, GetValue(i), GetValue(j), GetValue(x));
+                        Directory.CreateDirectory(path2);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ branches/1.0.0/NF.Web/default_old.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace newsflippers
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string cacheKey = Utility.GetCacheKey();
+                if (Cache[cacheKey] != null) return;
+
+                DateTime dt = Utility.LocalDate();
+
+                List<CaptureWebPage> capturedWebPages = NewsManager.GetCaptureWebPages();
+                if (capturedWebPages.Count == 0)
+                {
+                    dt = Utility.LocalDate().AddDays(-1);
+                    cacheKey = Utility.GetPreviousCacheKey();
+                    capturedWebPages = NewsManager.GetCaptureWebPages(Extensions.ToDateRef(dt));
+                }
+
+                string baseUrl = GetBaseURL();
+                //this.Label1.Text = string.Format("Date:{0}<br> Links:{1}", Utility.LocalDate().ToString(Utility.DATE_LONG_FORMAT), capturedWebPages.Count.ToString());
+                foreach (CaptureWebPage cp in capturedWebPages)
+                {
+                    cp.ImageName = string.Format("<a href='{1}' target=\"_blank\"><img border=\"0\" src=\"{0}\"   alt=\"Click here to read more...\" /></a>", string.Format("{0}pages/{1}/{2}", baseUrl, Extensions.ToNewsDateTimeFull(dt), cp.ImageName), cp.Url);
+                }
+
+                Cache[cacheKey] = capturedWebPages;
+            }
+        }
+
+        private string GetBaseURL()
+        {
+            string Port = Request.ServerVariables["SERVER_PORT"];
+            if (Port == null || Port == "80" || Port == "443")
+                Port = "";
+            else
+                Port = ":" + Port;
+
+            string Protocol = Request.ServerVariables["SERVER_PORT_SECURE"];
+            if (Protocol == null || Protocol == "0")
+                Protocol = "http://";
+            else
+                Protocol = "https://";
+
+
+            // *** Figure out the base Url which points at the application's root
+            string path = Protocol + Request.ServerVariables["SERVER_NAME"] +
+                                        Port + Request.ApplicationPath;
+
+            return path;
+        }
+    }
+}
